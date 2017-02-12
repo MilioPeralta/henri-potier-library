0cde10bf5c483ba25950bf8b1a39c884a239e5b1
Milio Peralta <milio.peralta@gmail.com>
2017-02-12 15:07:31 +0100

    [CLEAN]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { BookComponent } from './components/book/book.component';
-import { BookListComponent } from './components/book-list/book-list.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { SearchBarComponent } from './components/search-bar/search-bar.component';
-import { BasketComponent } from './components/basket/basket.component';
+import {AppComponent} from './app.component';
+import {BookComponent} from './components/book/book.component';
+import {BookListComponent} from './components/book-list/book-list.component';
+import {HeaderComponent} from './components/header/header.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {SearchBarComponent} from './components/search-bar/search-bar.component';
+import {BasketComponent} from './components/basket/basket.component';
 import {MaterialModule} from "@angular/material";
-import { CommaSeparatorPipe } from './pipe/comma-separator.pipe';
 
 @NgModule({
   declarations: [
@@ -21,8 +20,7 @@ import { CommaSeparatorPipe } from './pipe/comma-separator.pipe';
     HeaderComponent,
     FooterComponent,
     SearchBarComponent,
-    BasketComponent,
-    CommaSeparatorPipe
+    BasketComponent
   ],
   imports: [
     BrowserModule,
@@ -33,4 +31,5 @@ import { CommaSeparatorPipe } from './pipe/comma-separator.pipe';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
