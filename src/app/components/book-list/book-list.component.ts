f01efbd17b169d5c6c3837bc0b3d4698bcff643c
Milio Peralta <milio.peralta@gmail.com>
2017-02-12 14:16:03 +0100

    [ADD] basket, book, book-list, footer, header, search-bar components

---
+++ src/app/components/book-list/book-list.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit} from '@angular/core';
+import {BooksService} from "../../services/books.service";
+import {Book} from "../../classes/book";
+import {Observable} from "rxjs";
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css'],
+  providers: [BooksService]
+})
+export class BookListComponent implements OnInit {
+  private books: Observable<Book[]>;
+  private errorMessage: string;
+
+  constructor(private booksService: BooksService) {
+  }
+
+  ngOnInit() {
+    this.getBooks()
+  }
+
+  getBooks() {
+    this.books = this.booksService.getBooks();
+  }
+
+}
