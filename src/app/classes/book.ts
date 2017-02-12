a980f0b8f44bf0bb742ad17a3fd4ba400012d3bd
Milio Peralta <milio.peralta@gmail.com>
2017-02-12 14:15:24 +0100

    [ADD] book type

---
+++ src/app/classes/book.ts
@@ -0,0 +1,17 @@
+/**
+ * Class representation of a book
+ * Example :
+ * {
+ *    "isbn": "c8fabf68-8374-48fe-a7ea-a00ccd07afff",
+ *    "title": "Henri Potier à l'école des sorciers",
+ *    "price": 35,
+ *    "cover": "http://henri-potier.xebia.fr/hp0.jpg"
+ * }
+ *
+ */
+export class Book {
+  isbn: string;
+  title: string;
+  price: number;
+  cover: string;
+}
