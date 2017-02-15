225ed7a1ecec177a132eb3efcc002e9efdfa7728
Milio Peralta <milio.peralta@gmail.com>
2017-02-15 23:22:53 +0100

    [ADD] book quantity

--- src/app/classes/book.ts
+++ src/app/classes/book.ts
@@ -5,13 +5,18 @@
  *    "isbn": "c8fabf68-8374-48fe-a7ea-a00ccd07afff",
  *    "title": "Henri Potier à l'école des sorciers",
  *    "price": 35,
- *    "cover": "http://henri-potier.xebia.fr/hp0.jpg"
+ *    "cover": "http://henri-potier.xebia.fr/hp0.jpg",
+ *    "synopsis" : [{"Lorem Ipsum...."},{"Lorem Ipsum...."}]
  * }
+ *
+ * The quantity is used to handle the quantity added to the basket
  *
  */
 export class Book {
   isbn: string;
   title: string;
   price: number;
+  quantity: number;
   cover: string;
+  synopsis: string[];
 }
